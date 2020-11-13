83b3a576aefd45df36382cef81388857768d4900
repacksters <74412003+repacksters@users.noreply.github.com>
2020-11-14 01:47:26 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,10 +1,16 @@
-echo "[setup] Adding Trackers...."
-tracker_list=$(curl -s https://trackerslist.com/all_aria2.txt)
-echo "bt-tracker=$tracker_list" >> aria2c.conf
-
+# Install aria2c static binary
+echo "[setup] Installing Aria2c...."
+wget -q https://github.com/CypherpunkSamurai/TARclonef/raw/master/aria2-1.35.0-static-linux-amd64_patch_P3TERX.tar.gz
+tar xf aria2-1.35.0-static-linux-amd64_patch_P3TERX.tar.gz
+chmod 755 aria2c
 
 export MAX_DOWNLOAD_SPEED=0
-export MAX_CONCURRENT_DOWNLOADS=4
-aria2c --conf-path=aria2c.conf \
+tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',')
+export MAX_CONCURRENT_DOWNLOADS=7
+./aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
+   --max-connection-per-server=10 --rpc-max-request-size=1024M \
+   --bt-tracker="[$tracker_list]" --bt-max-peers=0 --seed-time=0.01 --min-split-size=10M \
+   --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
+   --peer-id-prefix=-qB4250- --user-agent==qBittorrent/4.2.5
